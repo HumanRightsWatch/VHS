b6c2e1fbb4a047f57c40ce9872ecb83a92aec346
U039b <git@0x39b.fr>
2021-11-23 15:25:27 +0100

    Initial commit

---
+++ video_downloading_platform/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from video_downloading_platform.users.models import User
+from video_downloading_platform.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
