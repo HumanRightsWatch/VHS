1f9103f72f4c465c2086508d0493f27100a6b724
U039b <git@0x39b.fr>
2022-01-28 13:43:22 +0100

    i18n

--- video_downloading_platform/core/models.py
+++ video_downloading_platform/core/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.conf import settings
 from django.db import models
 from django.urls import reverse_lazy
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django_q.humanhash import HumanHasher
 from django.core.validators import URLValidator
 from django_q.tasks import async_task
